c67aeec0432c2c9f747b0f7ca9f9b1b9928401eb
aidanakmatova13 <akmatova.don@gmail.com>
2021-08-12 19:10:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const valueList = document.querySelector('.value-list')
+const input = document.querySelector('.input')
+const resultInput = document.querySelector('.result')
+const getBtn = document.querySelector('.btn')
+const sign = document.querySelector('.sign')
+
+
+getBtn.addEventListener('click', ()=>{
+    if (valueList.value === 'EUR'){
+        fetch('https://api.exchangerate.host/latest?base=kgs')
+            .then(res => res.json())
+            .then(data => resultInput.value =  (data.rates.EUR*input.value).toFixed(2))
+        sign.textContent= '€'
+    } else if (valueList.value === 'USD'){
+        fetch('https://api.exchangerate.host/latest?base=kgs')
+            .then(res => res.json())
+            .then(data => resultInput.value = (data.rates.USD*input.value).toFixed(2))
+        sign.textContent= '$'
+    } else if (valueList.value === 'RU'){
+        fetch('https://api.exchangerate.host/latest?base=kgs')
+            .then(res => res.json())
+            .then(data => resultInput.value = (data.rates.RUB*input.value).toFixed(2))
+        sign.textContent= '₽'
+    }
+})
