f1fc22aa701f0f4142c4767925bd7d2787e64412
aidanakmatova13 <akmatova.don@gmail.com>
2021-08-13 15:32:49 +0300

    delete bag

--- script.js
+++ script.js
@@ -34,24 +34,3 @@
 // })
 // )
 
-
-
-
-const row = document.querySelector('.row')
-
-fetch('https://restcountries.eu/rest/v2/all')
-    .then(res => res.json())
-    .then(data => {
-        data.forEach(item => {
-            row.innerHTML += `<div class="col-3"> 
-            <div class="flag-img">
-            <img src="${item.flag}" alt="" class="img" > <br>
-            </div>
-           Cauntry Name: ${item.name} <br>
-           Capital Name: ${item.capital}<br>
-           Population: ${item.population}
-             </div>`
-        })
-        console.log(data)
-    })
-
