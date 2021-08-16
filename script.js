13f4b470735ebda55192ea3bb75c90d413ae5728
aidanakmatova13 <akmatova.don@gmail.com>
2021-08-16 14:27:33 +0300

    fix code

--- script.js
+++ script.js
@@ -34,3 +34,33 @@
 // })
 // )
 
+
+// const money = document.querySelector('.money')
+// const btn = document.querySelector('.btn')
+// const moneys = document.querySelector('.moneys')
+//
+// moneys.innerHTML = []
+// btn.addEventListener('click', () =>{
+//     let moneys = [100,50,25,5,1]
+//
+// })
+
+
+function get(amount){
+    const moneyList = [100,50,20,10]
+    const result = []
+
+    if (amount > 0){
+        for (let i = 0; i <moneyList.length; i ++){
+            let note = moneyList[i]
+            while (amount - note >= 0) {
+                amount -= note
+                result.push(note)
+            }
+        }
+    } else {
+        console.log('ENTER NEW AMOUNT!')
+    }
+    return result
+}
+console.log(get(250))
